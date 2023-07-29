c8db89d69e8d99969697a267fc8742a1b38c2937
Giovanni Gonzaga <giovannifgonzaga@gmail.com>
2023-07-29 11:24:08 -0500

    create build-a-blend frontend

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,107 @@
+'use client'
 import Image from 'next/image'
+import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react';
+import { ReadableStreamDefaultReadResult } from 'stream/web';
+import IconBoxArrowInUp from './arrow';
+import SubmitIcon from '@/submit-icon';
+
+
 
 export default function Home() {
+  const [input, setInput] = useState<string>("");
+  const [streamedData, setStreamedData] = useState<string>("");
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    setLoading(true);
+    fetch("http://127.0.0.1:8000/stream", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ content: input }),
+    }).then(res => res.json())
+      .then(data => {
+        const formattedText = data.response.response.split('\n').map((item: string, i: number) => <div key={i}>{item}</div>);
+        setStreamedData(formattedText)
+        setLoading(false)
+      })
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setInput(event.target.value);
+  }
+
+  const handleSuggestion = (suggestion: string) => {
+    setInput(suggestion.substring(1, suggestion.length - 1))
+  }
+
+  const Suggestion = () => {
+    const suggestions = [
+      "'I need to get rid of bacterial infections'",
+      "'I feel depressed and anxious all the time'",
+      "'I need to gain muscle mass and improve physical performance'",
+      "'I need a natural nootropic to enhance my memory and learning'",
+      "'My hair is thinning and receding'",
+      "'I want to be more confident and get rid of my social anxiety'",
+      "'I want to lose weight quickly'"
+    ];
+
+    return (
+      <div className="mt-4">
+        <h1 className="text-center">Examples</h1>
+        <ul>
+          {
+            suggestions.map((item: string, index: number) => (
+                <li key={index}
+                  onClick={(e) => handleSuggestion(item)}
+                  className="bg-black border rounded-lg px-4 py-3 w-full border-neutral-700 mt-2 transition duration-200 ease-in-out transform hover:scale-105 cursor-pointer hover:bg-neutral-800">{item}<IconBoxArrowInUp className="inline text-xl ml-4" /></li>
+                
+            ))
+          }
+        </ul>
+      </div>
+    )
+  }
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className="flex flex-col bg-black text-white justify-center items-center min-h-screen p-4 ">
+      <form onSubmit={handleSubmit} className="max-w-xl w-full">
+        <h1 className='text-center text-2xl mb-4'>Build-A-Blend</h1>
+        <label>
+          <div className="flex items-center justify-between bg-black border rounded-lg px-4 py-3 w-full border-neutral-700">
+            <input
+              type="text"
+              value={input}
+              onChange={handleChange}
+              className='bg-black w-full pr-2 focus:outline-none focus:ring-0'
+              placeholder='Describe a symptom or a desired outcome in health or life...'
             />
-          </a>
-        </div>
-      </div>
+            <button type='submit'>
+              <SubmitIcon className="h-4 w-4" />
+            </button>
+            
+          </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        </label>
+        {
+          !loading && streamedData.length == 0 && <Suggestion />
+        }
+      </form>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      {
+        loading && <span className="loader"></span>
+      }
+      {
+        streamedData.length > 0 && !loading && (
+          <div className="max-w-xl max-h-96 border rounded-lg overflow-auto py-3 px-4  border-neutral-700  mt-4 leading-8">
+            <div className="w-full h-full ">
+              {streamedData}
+            </div>
+          </div>
+        )
+      }
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
